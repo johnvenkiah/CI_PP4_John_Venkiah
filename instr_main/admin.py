d65f95c401c2b4623497edb5abd53b2969f6cf05
johnvenkiah <contact@johnvenkiah.com>
2021-10-23 16:17:33 +0000

    create AdAdmin class in admin.py

--- instr_main/admin.py
+++ instr_main/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from .models import Ad
 
-admin.site.register(Ad)
+
+@admin.register(Ad)
+class AdAdmin(ModelAdmin):
+
+    list_display = ('title', 'slug', 'price', 'seller', 'created_on', 'sold')
+    search_fields = ['title', 'description']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('sold', 'created_on')
