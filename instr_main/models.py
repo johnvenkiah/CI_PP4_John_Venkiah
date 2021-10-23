6eba4ada80590cc99383d9578b796721deed32cd
johnvenkiah <contact@johnvenkiah.com>
2021-10-23 15:30:09 +0000

    migrate ad model and successfully create ad from admin panel

--- instr_main/models.py
+++ instr_main/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
-class Ad(models.model):
+class Ad(models.Model):
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     seller = models.ForeignKey(
@@ -14,7 +14,7 @@ class Ad(models.model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     created_on = models.DateTimeField(auto_now_add=True)
     sold = models.BooleanField(default=False)
-    saved = models.ManyToManyField(User, related_name='saved', blank=True)
+    saved = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
