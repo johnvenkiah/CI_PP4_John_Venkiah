a4913de492060a411dcb3684832aa345d503ed33
johnvenkiah <contact@johnvenkiah.com>
2021-10-23 14:51:09 +0000

    Heroku deployment successful, add Ad model to models.py

--- instr_main/models.py
+++ instr_main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Ad(models.model):
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    seller = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='ads'
+    )
+    description = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    created_on = models.DateTimeField(auto_now_add=True)
+    sold = models.BooleanField(default=False)
+    saved = models.ManyToManyField(User, related_name='saved', blank=True)
