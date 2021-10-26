3921cf1dbe6f6fb92de5b57a86c997d3a18ffc6b
johnvenkiah <contact@johnvenkiah.com>
2021-10-26 23:50:08 +0000

    refine models even more, make migrations

--- instr_main/models.py
+++ instr_main/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 import boto3
-# from cloudinary.models import CloudinaryField
 
 
 class Ad(models.Model):
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     seller = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='ads'
+        User, on_delete=models.CASCADE
     )
-    description = models.TextField()
+    description = models.CharField(max_length=800)
     featured_image = models.ImageField('image', default='placeholder')
-    price = models.DecimalField(max_digits=6, decimal_places=2)
     created_on = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     sold = models.BooleanField(default=False)
     saved = models.BooleanField(default=False)
 
@@ -25,7 +24,16 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=200, unique=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(default='placeholder', upload_to='profile_pics')
+    image = models.ImageField(default='placeholder')
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'{self.user.username}: Profile'
+
+
+class Category(models.Model):
+    title = models.CharField('title', max_length=100)
+    slug = models.SlugField(blank=True, null=True)
+    image = models.ImageField(default='placeholder')
+
+    def __str__(self):
+        return self.title
