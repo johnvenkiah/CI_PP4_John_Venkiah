b073f8e0548f6ee06c2f932893e62de35f75e6e3
johnvenkiah <contact@johnvenkiah.com>
2021-10-27 14:24:10 +0000

    re-add ads var in Profile and Categories models, rename Category to Categories, add location and geolocation fields to Ad and Profile classes, run migrations

--- instr_main/admin.py
+++ instr_main/admin.py
@@ -1,21 +1,33 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
-from .models import Ad, Profile
+from .models import Ad, Profile, Categories
 
 
 @admin.register(Ad)
 class AdAdmin(ModelAdmin):
 
-    list_display = ('title', 'slug', 'price', 'seller', 'created_on', 'sold')
+    list_display = (
+        'title', 'slug', 'price', 'seller', 'created_on',
+        'sold', 'location', 'category'
+        )
     search_fields = ['title', 'description']
     prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('sold', 'created_on')
+    list_filter = ('sold', 'created_on', )
 
 
 @admin.register(Profile)
-class AdAdmin(ModelAdmin):
+class ProfileAdmin(ModelAdmin):
 
     list_display = ('user', 'slug', 'created_on')
     search_fields = ['user']
     prepopulated_fields = {'slug': ('user',)}
-    list_filter = ('user', 'created_on')
+    list_filter = ('user', 'created_on', )
+
+
+@admin.register(Categories)
+class CategoriesAdmin(ModelAdmin):
+
+    list_display = ('title', 'ads')
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('title', )
--- instr_main/models.py
+++ instr_main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import boto3
+from django_google_maps import fields as map_fields
 
 
 class Ad(models.Model):
@@ -9,10 +10,15 @@ class Ad(models.Model):
     seller = models.ForeignKey(
         User, on_delete=models.CASCADE
     )
+    category = models.ForeignKey(
+        'Categories', on_delete=models.SET_NULL, null=True
+    )
     description = models.CharField(max_length=800)
     featured_image = models.ImageField('image', default='placeholder')
     created_on = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    location = map_fields.AddressField(max_length=200)
+    geolocation = map_fields.GeoLocationField(max_length=100)
     sold = models.BooleanField(default=False)
     saved = models.BooleanField(default=False)
 
@@ -23,15 +29,19 @@ class Ad(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=200, unique=True)
+    ads = models.ForeignKey(Ad, on_delete=models.SET_NULL, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    location = map_fields.AddressField(max_length=200)
+    geolocation = map_fields.GeoLocationField(max_length=100)
 
     def __str__(self):
         return f'{self.user.username}: Profile'
 
 
-class Category(models.Model):
+class Categories(models.Model):
     title = models.CharField('title', max_length=100)
-    slug = models.SlugField(blank=True, null=True)
+    slug = models.SlugField(max_length= 200, unique=True)
+    ads = models.ForeignKey(Ad, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
