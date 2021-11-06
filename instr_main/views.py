d9414a9bd6a776bfaa6eb3f91ef8da74d7c6c90d
johnvenkiah <contact@johnvenkiah.com>
2021-11-06 17:08:56 +0000

    add /profile/ to get_success_url in AdCreateView

--- instr_main/views.py
+++ instr_main/views.py
@@ -252,7 +252,7 @@ class AdCreateView(CreateView):
         return super(AdCreateView, self).form_invalid(form)
 
     def get_success_url(self):
-        return 'profile/'
+        return '/profile/'
 
     # def get(self, request):
     #     context = {
