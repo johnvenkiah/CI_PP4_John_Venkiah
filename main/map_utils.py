f3a3169098e898c6cfc71e7e134aa07bbf5bf45f
johnvenkiah <contact@johnvenkiah.com>
2021-11-22 14:33:35 +0000

    create get_city_by_lat_long fn in main.maputils.py

--- main/map_utils.py
+++ main/map_utils.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import geocoder
 
 
 def get_lat_long_by_address(valid_address):
@@ -11,12 +12,23 @@ def get_lat_long_by_address(valid_address):
     r = requests.get(endpoint)
     if r.status_code not in range(200, 299):
         return None, None
-    '''
-    This try block incase any of our inputs are invalid. This is done
-    instead of actually writing out handlers for all kinds of responses.
-    '''
-    results = r.json()['results'][0]
-    lat = results['geometry']['location']['lat']
-    lng = results['geometry']['location']['lng']
-
+    try:
+        '''
+        This try block incase any of our inputs are invalid. This is done
+        instead of actually writing out handlers for all kinds of responses.
+        '''
+        results = r.json()['results'][0]
+        lat = results['geometry']['location']['lat']
+        lng = results['geometry']['location']['lng']
+    except:
+        pass
     return [lat, lng]
+
+
+def get_city_by_lat_long(coords):
+    city = [
+        geocoder.google(
+            [coord[0], coord[1]], method='reverse'
+        ).city for coord in coords
+    ]
+    return city
--- main/views.py
+++ main/views.py
@@ -8,20 +8,14 @@ import geocoder
 
 from ads.models import Ad
 from .categories import category_dict
-from .map_utils import get_lat_long_by_address
+from .map_utils import get_lat_long_by_address, get_city_by_lat_long
 
 
 class HomeView(TemplateView):
     def get_context_data(self):
         ads = Ad.objects.all()
         coords = [get_lat_long_by_address(item.location) for item in ads]
-        area_list = [
-            geocoder.google(
-                [
-                    coord[0], coord[1]
-                ], method='reverse'
-            ).city for coord in coords
-        ]
+        area_list = get_city_by_lat_long(coords)
         area_list += ['- All of Great Britain -']
         context = {
             'category_dict': category_dict,
