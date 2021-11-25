d84c4a2e22e6679925639fb4172c2abed98941cf
johnvenkiah <contact@johnvenkiah.com>
2021-11-25 14:56:25 +0000

    models.Ad: change price field to PosititveIntegerField, add validator for max value

--- ads/models.py
+++ ads/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
+from django.core.validators import MaxValueValidator
 
 from main.map_utils import get_lat_long_by_address, get_city_by_lat_long
 
@@ -17,7 +18,9 @@ class Ad(models.Model):
     category = models.CharField(max_length=250)
     description = models.CharField(max_length=800)
     created_on = models.DateTimeField(auto_now_add=True)
-    price = models.IntegerField(max_length=14)
+    price = models.PositiveIntegerField(
+        validators=[MaxValueValidator(99999999)]
+    )
     location = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
     sold = models.BooleanField(default=False)
