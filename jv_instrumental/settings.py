7abd11af97f2d59d793f532211f0295cd495240e
johnvenkiah <contact@johnvenkiah.com>
2021-11-27 01:11:34 +0000

    Add STATIC_ROOT to settings.py

--- jv_instrumental/settings.py
+++ jv_instrumental/settings.py
@@ -195,6 +195,7 @@ if 'USE_AWS' in os.environ:
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
+    STATIC_ROOT = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
