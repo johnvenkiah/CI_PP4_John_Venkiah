748e301dd1ae7bb27eab5d8e08e8edd94bc29075
johnvenkiah <contact@johnvenkiah.com>
2021-11-28 09:30:20 +0000

    style: index and profile pages for tablet screen size

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -6,5 +6,5 @@ app_name = 'user_profile'
 
 urlpatterns = [
     path('edit_profile/', views.edit_profile, name='edit_profile'),
-    path('<slug:user>/', views.ProfileView.as_view(), name='profile'),
+    path('profile/<slug:user>/', views.ProfileView.as_view(), name='profile'),
  ]
