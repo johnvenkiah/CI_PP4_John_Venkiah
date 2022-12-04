8534f7f5ac340ce5013cdc6ff0c7b416e2d2a183
johnvenkiah <contact@johnvenkiah.com>
2022-12-04 21:56:26 +0000

    attempt to fix static errors js and favicon

--- main/map_utils.py
+++ main/map_utils.py
@@ -1,30 +1,9 @@
-import os
-import requests
 import geocoder
 
 
 def get_lat_long_by_address(valid_address):
-    lat, lng = None, None
-    api_key = os.environ.get('GOOGLE_API_KEY')
-    base_url = "https://maps.googleapis.com/maps/api/geocode/json"
-    endpoint = f"{base_url}?address={valid_address}&key={api_key}"
-
-    r = requests.get(endpoint)
-    if r.status_code not in range(200, 299):
-        return None, None
-    try:
-        '''
-        This try block incase any of our inputs are invalid. This is done
-        instead of actually writing out handlers for all kinds of responses.
-        '''
-        results = r.json()['results'][0]
-        lat = results['geometry']['location']['lat']
-        lng = results['geometry']['location']['lng']
-    except Exception:
-        pass
-
-    # g = geocoder.google(valid_address)
-    return [lat, lng]
+    g = geocoder.google(valid_address)
+    return [g.latlng[0], g.latlng[1]]
 
 
 def get_city_by_lat_long(coords):
--- static/js/autocomplete_location.js
+++ static/js/autocomplete_location.js
@@ -11,56 +11,56 @@ let autocomplete;
 let address1Field;
 
 function initAutocomplete() {
-  address1Field = document.querySelector("#id_location");
-
-  // Create the autocomplete object, restricting the search predictions to
-  // addresses in the US and Canada.
-  autocomplete = new google.maps.places.Autocomplete(address1Field, {
-    componentRestrictions: { country: ["gb"] },
-    fields: ["address_components", "geometry"],
-    types: ["address"],
-  });
-
-  // When the user selects an address from the drop-down, populate the
-  // address fields in the form.
-  autocomplete.addListener("place_changed", fillInAddress);
+    address1Field = document.querySelector("#id_location");
+    
+    // Create the autocomplete object, restricting the search predictions to
+    // addresses in the US and Canada.
+    autocomplete = new google.maps.places.Autocomplete(address1Field, {
+        componentRestrictions: { country: ["gb"] },
+        fields: ["address_components", "geometry"],
+        types: ["address"],
+    });
+    
+    // When the user selects an address from the drop-down, populate the
+    // address fields in the form.
+    autocomplete.addListener("place_changed", fillInAddress);
 }
 
 function fillInAddress() {
     // Get the place details from the autocomplete object.
     const place = autocomplete.getPlace();
     let address1 = "";
-
+    
     // Get each component of the address from the place details,
     // and then fill-in the corresponding field on the form.
     // place.address_components are google.maps.GeocoderAddressComponent objects
     // which are documented at http://goo.gle/3l5i5Mr
     for (const component of place.address_components) {
         const componentType = component.types[0];
-
+        
         switch (componentType) {
             case "street_number": {
                 address1 = `${component.long_name} ${address1}`;
                 break;
             }
-
+            
             case "route": {
                 address1 += `${component.long_name},`;
                 break;
             }
-
+            
             case "postal_town": {
                 address1 += ` ${component.long_name}`;
                 break;
             }
-
+            
             case "country": {
                 address1 += ` ${component.long_name}${country}`;
                 break;
             }
         }
-
+        
         address1Field.value = address1;
-
+        
     }
 }
